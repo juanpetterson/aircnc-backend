37a136abc565cfdb36318285ac2dafa4caa151bb
Juan Petterson <juan.petterson@gmail.com>
2019-11-30 17:21:04 -0300

    Implemented booking and dashboard controllers and models

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const routes = require('./routes')
 const app = express()
 
 const user = process.env.MONGO_DB_ATLAS_USER
-const password = process.env.MONGO_DB_ATLAS_USER
+const password = process.env.MONGO_DB_ATLAS_PASSWORD
 
 mongoose.connect(`mongodb+srv://${user}:${password}@cluster0-hfyur.gcp.mongodb.net/aircnc?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
