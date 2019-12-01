dc9d4e09ac004a2954b4df8927dce51f694fda04
Juan Petterson <juan.petterson@gmail.com>
2019-12-01 15:22:26 -0300

    Added thumbnail_url virtual model

--- src/server.js
+++ src/server.js
@@ -1,19 +1,25 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const routes = require('./routes')
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-const user = process.env.MONGO_DB_ATLAS_USER
-const password = process.env.MONGO_DB_ATLAS_PASSWORD
+const user = process.env.MONGO_DB_ATLAS_USER;
+const password = process.env.MONGO_DB_ATLAS_PASSWORD;
 
-mongoose.connect(`mongodb+srv://${user}:${password}@cluster0-hfyur.gcp.mongodb.net/aircnc?retryWrites=true&w=majority`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(
+  `mongodb+srv://${user}:${password}@cluster0-hfyur.gcp.mongodb.net/aircnc?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
-app.use(express.json())
-app.use(routes)
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
 
-app.listen(3333)
+app.listen(3333);
